7eb37b4a429c0f2d342d70dbcb49c47e7271b766
Sambasivarao <samba.pendela@gmail.com>
2022-05-30 21:34:46 +0530

    code

--- arrays.js
+++ arrays.js
@@ -150,6 +150,150 @@ console.log(
 //1500
 
 
+let arr17 = ["ReactJS","to","Welcome"];
+console.log(
+    arr17.reduceRight((firstElement,nextElment)=>{
+        return firstElement+" "+nextElment;
+    })
+);          //Welcome to ReactJS
+
+
+
+let arr18 = [[1],[2],[3]];
+console.log(
+    arr18.flat(1)
+);
+//[ 1, 2, 3 ]
+
+
+let arr19 = [[1,2],[3,4],[5,6]];
+console.log(
+    arr19.flat(1).reduce((firstElement,nextElment)=>{
+        return firstElement+nextElment;
+    })
+);
+//21
+
+let arr20 = [[[[[[[[[[[1]]]]]]]]]],[[[[[[[[[[[[[[[[[[[[[[[[2]]]]]]]]]]]]]]]]]]]]]]]]];
+console.log( arr20.flat(Infinity) );
+//[ 1, 2 ]
+
+
+let arr21 = [1,2,3,4,5];
+let arr22 = ["one","two","three","four","five"];
+//[[1,"one"],[2,"two"],[3,"three"],[4,"four"],[5,"five"]]
+//[1,"one",2,"two",3,"three",4,"four",5,"five"]
+console.log(
+    arr21.flatMap((element,index)=>{
+        return [element,arr22[index]]
+    })
+);
+
+
+let arr23 = [1,2,3,1,2,3];
+arr23.forEach((element,index)=>{
+    console.log( arr23.indexOf(element) );
+});
+//0 1 2 0 1 2
+
+let arr24 = ["Hello","Welcome","Hello","Welcome","Hi"];
+arr24.forEach((element,index)=>{
+    console.log( arr24.indexOf(element) );
+});
+//0 1 0 1 4
+
+
+let arr25 = [10,20,30,10,20,30,40];
+console.log(
+    arr25.filter((element,index)=>{
+        return arr25.indexOf(element) == index;
+    })
+);
+//[ 10, 20, 30, 40 ]
+
+
+//sort()
+//ascending order / decending order
+let arr26 = [1,5,2,4,3];
+//[1,2,3,4,5]  //ascending
+//[5,2,3,2,1]  //decending
+
+console.log(
+    arr26.sort((num1,num2)=>{
+        return num1-num2;
+    })
+);
+//[ 1, 2, 3, 4, 5 ]
+
+console.log(
+    arr26.sort((num1,num2)=>{
+        return num1-num2;
+    })[1]
+);
+//2
+
+
+
+console.log(
+    arr26.sort((num1,num2)=>{
+        return num2-num1;
+    })
+);
+//[ 5, 4, 3, 2, 1 ]
+
+console.log(
+    arr26.sort((num1,num2)=>{
+        return num2-num1;
+    })[1]
+);
+//4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
