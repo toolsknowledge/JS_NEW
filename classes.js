4e1739e989c39d7e32dc4fe70d8ef02a9760d5a9
Sambasivarao <samba.pendela@gmail.com>
2022-06-30 07:02:20 +0530

    code

---
+++ classes.js
@@ -0,0 +1,53 @@
+//collection of variables and functions called as class
+//we will create classes by using "class" keyword
+//we will create object to the class by using new keyword
+// class class_one{
+//     sub_one = "ReactJS";
+//     sub_two = "NodeJS";
+//     sub_three = "MongoDB";
+// };
+// let obj = new class_one();
+// console.log( obj.sub_one );
+// console.log( obj.sub_two );
+// console.log( obj.sub_three );
+
+
+// class class_one{
+//     var_one = "Hello_1";
+//     func_one(){
+//         return this.var_one;
+//     }
+//     var_two = "Hello_2";
+//     func_two(){
+//         return this.var_two;
+//     }
+//     var_three = "Hello_3";
+//     func_three(){
+//         return this.var_three;
+//     }
+// }
+// let obj = new class_one();
+// console.log( obj.func_one(), obj.func_two(), obj.func_three() );
+//Hello_1 Hello_2 Hello_3
+
+
+class class_one{
+    #var_one = "Hello";
+}
+let obj = new class_one();
+console.log( obj.var_one );         //undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
